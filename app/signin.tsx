4dbeae19d74f0ea2c0a09dcc8c3c2437a9cf4b8b
devkush0999 <devesh2cse@gmail.com>
2024-12-26 20:35:39 +0530

    .

--- app/signin.tsx
+++ app/signin.tsx
@@ -1,39 +1,41 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { Link, router } from 'expo-router'
-import InputField from '@/components/InputField'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { Link, router } from "expo-router";
+import InputField from "@/components/InputField";
 
-type Props = {}
+type Props = {};
 
 const SignInScreen = (props: Props) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>SignIn Screen</Text>
+      <Text style={styles.title}>SignIn Screen d</Text>
       {/* <Link href={"/(tabs)"} asChild> */}
-        <TouchableOpacity onPress={() => {
+      <TouchableOpacity
+        onPress={() => {
           router.dismissAll();
-          router.push('/(tabs)');
-        }}>
-          <InputField placeholder='Email' autoCapitalize='none'/>
-          <InputField placeholder='password' />
-           </TouchableOpacity>
+          router.push("/(tabs)");
+        }}
+      >
+        <InputField placeholder="Email" autoCapitalize="none" />
+        <InputField placeholder="password" />
+      </TouchableOpacity>
       {/* </Link> */}
     </View>
-  )
-}
+  );
+};
 
-export default SignInScreen
+export default SignInScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 20,
   },
-  title : {
-    marginBottom: 20 ,
+  title: {
+    marginBottom: 20,
     fontSize: 24,
-    fontWeight: '600'
-  }
-})
+    fontWeight: "600",
+  },
+});
